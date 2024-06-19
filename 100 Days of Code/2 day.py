c10ada8c64947407e0945d3c9106381bbd896ec9
lambert <44629854+nicjlambert@users.noreply.github.com>
2024-06-19 13:58:53 +0000

    updated

---
+++ 100 Days of Code/2 day.py
@@ -0,0 +1,27 @@
+# Day 2
+# Beginner - Understanding Data Types and How to Manipulate Strings
+
+string = "Welcome to the tips calculator!"
+
+print(string)
+
+# first character
+print(string[0])
+
+# last character
+print(string[len(string) - 1])
+
+# string
+print("123" + "345")
+
+# int
+print(123 + 345)
+
+# float 
+
+3.14159
+
+# boolean
+
+True
+False
