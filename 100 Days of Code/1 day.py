3ea0028d99f1e213b26d28f0a4e80675a19a6bbc
lambert <44629854+nicjlambert@users.noreply.github.com>
2024-06-23 01:52:48 +0000

    updated

--- 100 Days of Code/1 day.py
+++ 100 Days of Code/1 day.py
@@ -1,7 +1,6 @@
 # Day 1
 # Beginner - Working with variables in Python to manage data
 
-#print function
 print("Hello World!")
 
 print('Day 1 - Python Print Function\nThe function is declared like this: \nprint("what to print")')
@@ -9,17 +8,9 @@ print('Day 1 - Python Print Function\nThe function is declared like this: \nprin
 print("Hello, " + input("What is your name? ") )
 
 # Write a program that prints the number of characters in a user's name.
-# You might need to Google for a function that calculates the length of a string
+print(len(input("What is your name? ")))
 
-print(
-    len(
-        input("What is your name? ")
-    )
-)
-
-#  assign name inputted as a variable called name
+# Assign name inputted as a variable called name
 name = input("What is your name? ")
 
-print(
-    len(name)
-)
+print(len(name))
